74760a762a456f02629fd22073f6d893ddc94f46
Joshua Davian <joshuadavian@gmail.com>
2025-01-03 18:28:29 +0700

    feat: Room Model & Migration

---
+++ database/migrations/2025_01_03_112323_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('boarding_house_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('room_type');
+            $table->double('square_feet');
+            $table->bigInteger('price_per_month');
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
