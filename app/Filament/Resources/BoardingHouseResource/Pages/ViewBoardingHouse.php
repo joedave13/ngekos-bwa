e1ebae0fbf8db4b8b3520ef22a244608c82e5cde
Joshua Davian <joshuadavian@gmail.com>
2025-01-05 21:08:03 +0700

    feat: Boarding House view page infolist

--- app/Filament/Resources/BoardingHouseResource/Pages/ViewBoardingHouse.php
+++ app/Filament/Resources/BoardingHouseResource/Pages/ViewBoardingHouse.php
@@ -4,6 +4,10 @@ namespace App\Filament\Resources\BoardingHouseResource\Pages;
 
 use App\Filament\Resources\BoardingHouseResource;
 use Filament\Actions;
+use Filament\Infolists\Components\Grid;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewBoardingHouse extends ViewRecord
@@ -19,5 +23,25 @@ class ViewBoardingHouse extends ViewRecord
             Actions\DeleteAction::make()
                 ->icon('heroicon-m-trash'),
         ];
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist->schema([
+            TextEntry::make('name'),
+            TextEntry::make('slug'),
+            Grid::make(3)->schema([
+                TextEntry::make('city.name'),
+                TextEntry::make('category.name'),
+                TextEntry::make('price')->prefix('IDR '),
+            ]),
+            ImageEntry::make('thumbnail')
+                ->size(250)
+                ->columnSpanFull(),
+            TextEntry::make('description')
+                ->html(),
+            TextEntry::make('address')
+                ->html(),
+        ]);
     }
 }
